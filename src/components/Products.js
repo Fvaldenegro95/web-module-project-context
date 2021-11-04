79c6cad938d0677d0d783d51a2c81ab92b39fa39
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-04 19:22:58 -0400

    d

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 // Components
 import Product from './Product';
+import { ProductContext } from '../context/ProductContext';
+import { useContext } from 'react';
 
-const Products = props => {
+const Products = () => {
+
+	const {products, addItem} = useContext(ProductContext)
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 // Components
 import Item from './ShoppingCartItem';
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
 const ShoppingCart = props => {
+	const cart = useContext(CartContext);
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
